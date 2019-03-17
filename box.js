cfcd5669ec54b27390c8375967ca868266a2de7c
Aruni Ranaweera <arunixr@gmail.com>
2019-03-17 17:48:52 +0000

    Automatically backed up by Learn

---
+++ box.js
@@ -0,0 +1,14 @@
+
+function weather(temp, wind) {
+  console.log( `Its is ${temp}  and the wind chill is ${wind}`)
+}
+
+weather(60,50)
+
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+}
+
+function add(x, y) {
+  return x + y
+}
